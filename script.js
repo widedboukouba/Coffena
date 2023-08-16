618604d4e3ac72436d4e1df617d4be17feb5895f
widedboukouba <widedboukouba64@gmail.com>
2023-08-16 15:02:36 +0200

    first

---
+++ script.js
@@ -0,0 +1,144 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+//like
+
+let likes=document.getElementsByClassName("like");
+for(let i=0; i<likes.length; i++){
+    let like=likes[i];
+    like.addEventListener("click", function(){
+        if(  like.firstChild.style.color=="white"){
+            like.firstChild.style.color="red";
+        }else{
+like.firstChild.style.color="white";
+        }
+
+    })
+}
+
+
+
+//formulaire
+function printError(Id, Msg) {
+    document.getElementById(Id).innerHTML = Msg;
+}
+
+function validateForm() {
+
+    var name = document.Form.name.value;
+    var email = document.Form.email.value;
+    var mobile = document.Form.mobile.value;
+    
+    
+
+    var nameErr = emailErr = mobileErr  = true;
+    
+
+    if(name == "") {
+        printError("nameErr", "Please enter your name");
+        var elem = document.getElementById("name");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+            var elem = document.getElementById("name");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+            var elem = document.getElementById("name");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+
+            
+        }
+    }
+    
+
+    if(email == "") {
+        printError("emailErr", "Please enter your email address");
+         var elem = document.getElementById("email");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+            var elem = document.getElementById("email");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+             var elem = document.getElementById("email");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+
+        }
+    }
+    
+ 
+    if(mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+        var elem = document.getElementById("mobile");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        var regex = /^[1-9]\d{9}$/;
+        if(regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else{
+            printError("mobileErr", "");
+            mobileErr = false;
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+        }
+    }
+    
+
+   
+    
+    
+    if((nameErr || emailErr || mobileErr ) == true) {
+       return false;
+    } 
+   
+};
+ 
